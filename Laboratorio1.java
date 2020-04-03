ad37907048fa35833d5b3bfff067260a0d4f8151
Agustin <arodrguezl3>
2020-04-03 13:40:09 -0300

    Se crean los archivos iniciales del proyecto

---
+++ Laboratorio1.java
@@ -0,0 +1,7 @@
+public class Laboratorio1{
+
+    public static void main(String[] args){
+        Laboratorio1 obj = new Laboratorio1();
+        System.out.println("Bienvenido al primer trabajo de la asignatura Estructura de Datos y Algoritmos");
+    }
+    }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Lab1
+laboratorio1
