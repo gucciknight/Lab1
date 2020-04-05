203aa79d2965a197dcb89d4c953e415be4f1d88d
root <root@localhost.localdomain>
2020-04-05 03:45:14 -0400

    Cambio de nombre al codigo de java para manejo de bases de datos

---
+++ Codigo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.utils.ArrayList;
+class Conversor{
+    public static void main(String [] args) throws IOException{
+    BufferedReader X = new BufferedReader(new InputStreamReade(System.in));
+    //categorias son 3 Descripcion | Categoria | Precio
+    String[] ListadoyDescripcion = new String[1];
+    int[] Precio = new int[];
+    System.out.println("Systema de datos comerciales \n
+    En nuestro menu le damos a ingresar las siguientes opciones\n
+    1.- Ingresar Datos\n
+    2.- Consultar Datos\n
+    3-. Eliminar Datos");
+    }}
