ea193b2369ac559c7f2b4f524e1b2208a16c4fec
root <root@localhost.localdomain>
2020-04-05 03:42:54 -0400

    eliminacion de archivo basura

--- Laboratorio1.java
+++
@@ -1,7 +0,0 @@
-public class Laboratorio1{
-
-    public static void main(String[] args){
-        Laboratorio1 obj = new Laboratorio1();
-        System.out.println("Bienvenido al primer trabajo de la asignatura Estructura de Datos y Algoritmos");
-    }
-    }
