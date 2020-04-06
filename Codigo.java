d7a9e0c3abe5476c232cf86e4b1f4526b020f4ab
root <root@localhost.localdomain>
2020-04-05 22:42:33 -0400

    avance de codigo

--- Codigo.java
+++ Codigo.java
@@ -2,8 +2,11 @@ import java.io.*;
 import java.utils.ArrayList;
 class Conversor{
     public static void main(String [] args) throws IOException{
-    BufferedReader X = new BufferedReader(new InputStreamReade(System.in));
+    BufferedReader X = new BufferedReader(new InputStreamReader(System.in));
     //categorias son 3 Descripcion | Categoria | Precio
+    File archivo = new File("Productors de Construccion.txt");
+    FileWriter Escritura = new FileWriter("Productors de Construccion.txt");
+    FileReader Lectura = new FileReader("Productors de Construccion.txt");
     String[] ListadoyDescripcion = new String[2];
     int[] Precio = new int[0];
     System.out.println("Systema de datos comerciales \n"+
@@ -17,8 +20,25 @@ class Conversor{
     option = Integer.parseInt(entrada);
     boolean candado = true;
     int Cantidad;
+    int Desicion;
+    int ID = Lectura.read();
+    int IDMAX = null;
+    String Descripcion;
+    String Categoria;
+    String Precio;
+    public static void BuscarPalabra(File archivo, String Descripcion) {
+    	try {
+    		if(archivo.exists()) {
+    			String LineaLeida;
+    			while((LineaLeida = X.readLine() != null)) {
+    				ID = ID + 1;
+    			}
+    		}
+    	}
+    }
     switch(option){
         case(1):
+        	archivo.canWrite(true);
         	System.out.println("Ha ingresado la opcione para ingresar datos\n"
         			+ "Ingrese cuantos datos desea ingresear");
         	entrada = X.readLine();
@@ -31,8 +51,20 @@ class Conversor{
 	        	System.out.println("Ingrese el Precio");
 	        	ListadoyDescripcion[2]= X.readLine();
         	}
+        	System.out.println("Esta seguro de guardar los cambios ?\n"
+        			+ "1.- SI\n"
+        			+ "2.- NO");
+        	entrada = X.readLine();
+        	Desicion = Integer.parseInt(entrada);
+        	if (Desicion == 1) {
+        		archivo.write(IDMAX + 1,",",ListadoyDescripcion[0],",",ListadoyDescripcion[1],",",ListadoyDescripcion[2]);
+        	}else {
+        		System.out.println("Ha eliminado todos los datos ingresados");
+        	}
+        	Escritura.close();
             break;
         case(2):
+        	archivo.canRead(true);
         	System.out.println("Ha ingresado la opcion para Buscar Datos\n"
         			+ "Ingrese la opcion por como desea realizar la busqueda\n"
         			+ "1.- Busqueda por Descripcion\n"
@@ -42,6 +74,9 @@ class Conversor{
         	option = Integer.parseInt(entrada);
         	switch(option) {
         	case(1):
+        		System.out.println("Ingrese la Descripcion del producto");
+        		Descripcion = X.readLine();
+        		BuscarPalabra(archivo, Descripcion);
         		break;
         	case(2):
         		break;
